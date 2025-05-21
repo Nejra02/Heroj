9a533486bea6b7e4ec72d2fd2bd3582db08c9a70
MadzakHataa <madzakhataa@gmail.com>
2025-05-21 09:48:14 +0200

    hash u user model

--- models/user_model.py
+++ models/user_model.py
@@ -1,8 +1,22 @@
 from sqlmodel import SQLModel, Field
+from typing import Optional
+import bcrypt
 
 class User(SQLModel, table=True):
-    user_id: int | None = Field(default=None, primary_key=True)
+    user_id: Optional[int] = Field(default=None, primary_key=True)
     username: str
-    password: str
+    password: str  # hashirani password
     role: str
     email: str
+
+    @property
+    def plain_password(self):
+        raise AttributeError("Lozinka se ne može čitati u običnom tekstu.")
+
+    @plain_password.setter
+    def plain_password(self, plain_text: str):
+        if not plain_text.startswith("$2b$"):
+            hashed = bcrypt.hashpw(plain_text.encode(), bcrypt.gensalt())
+            self.password = hashed.decode()
+        else:
+            self.password = plain_text
--- services/user_service.py
+++ services/user_service.py
@@ -1,5 +1,7 @@
+import bcrypt
 from sqlmodel import Session
 from sqlalchemy.orm import Session
+from database import SessionLocal
 from models.user_model import User
 from repositories import user_repositories
 from fastapi import HTTPException
@@ -11,3 +13,7 @@ def get_users(session: Session, offset: int = 0, limit: int = 100) -> list[User]
 def create_user_service(db: Session, user_data: dict):
     user_data["password"] = bcrypt_context.hash(user_data["password"])
     return user_repositories.create_user(db, user_data)
+
+def is_password_hashed(password):
+    # Pretpostavka: bcrypt hash počinje sa $2b$ i ima oko 60 karaktera
+    return password.startswith('$2b$') and len(password) >= 60
