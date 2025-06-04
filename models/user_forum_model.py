e9c0caaa79aa6c1fe3381be875cab0e630ad64f9
Emina Delić <e258delic@gmail.com>
2025-06-04 17:37:19 +0200

    opet bzvz promjena u models

--- models/user_forum_model.py
+++ models/user_forum_model.py
@@ -4,3 +4,4 @@ class User_forum(SQLModel, table=True):
     komentar_id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(default=None, foreign_key="user.user_id")
     objava_id: int = Field(default=None, foreign_key="forum.objava_id")
+    tekst_komentara: str
